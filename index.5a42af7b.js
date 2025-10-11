ca93dcfd41821babd68656c73896a09bb3c896e7
maharvladyslav <128148368+maharvladyslav@users.noreply.github.com>
2025-10-11 11:14:25 +0000

    Deploying to gh-pages from @ maharvladyslav/interno@224676075f60c0b7229fd3a410db688b42a2f349 🚀

---
+++ index.5a42af7b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-modal-open]"),t=document.querySelector("[data-modal]"),o=document.querySelector("[data-modal-close]"),d=()=>{t.classList.toggle("is-hidden"),document.body.classList.toggle("no__scroll")};e.addEventListener("click",d),o.addEventListener("click",d);
+//# sourceMappingURL=index.5a42af7b.js.map
---
+++ index.e5ea480c.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-modal-open]"),t=document.querySelector("[data-modal]"),o=document.querySelector("[data-modal-close]"),c=function(){t.classList.toggle("is-hidden"),document.body.classList.toggle("no__scroll")};e.addEventListener("click",c),o.addEventListener("click",c)}();
+//# sourceMappingURL=index.e5ea480c.js.map
