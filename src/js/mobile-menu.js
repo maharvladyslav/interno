a1985d25b38b22270e1f13ac8d0b92676b0c484e
Mahar Vladyslav <maharvladyslav@gmail.com>
2025-10-16 19:31:36 +0300

    Remove unused mobile menu and modal scripts; update header links for mobile compatibility

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -5,11 +5,13 @@ const links = document.querySelectorAll('[data-mobile-link]');
 const handleMenu = () => {
   menu.classList.toggle('is-open');
   menuBtn.classList.toggle('is-open');
+  document.body.classList.toggle('no__scroll');
 };
 links.forEach(link => {
   link.addEventListener('click', () => {
     menu.classList.remove('is-open');
     menuBtn.classList.remove('is-open');
+    document.body.classList.remove('no__scroll');
   });
 });
 
