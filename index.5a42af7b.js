47a9ad537f3598b6b95a77f105edee62bd6ecdc9
maharvladyslav <128148368+maharvladyslav@users.noreply.github.com>
2025-10-16 16:32:23 +0000

    Deploying to gh-pages from @ maharvladyslav/interno@a1985d25b38b22270e1f13ac8d0b92676b0c484e 🚀

--- index.5a42af7b.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-modal-open]"),t=document.querySelector("[data-modal]"),o=document.querySelector("[data-modal-close]"),d=()=>{t.classList.toggle("is-hidden"),document.body.classList.toggle("no__scroll")};e.addEventListener("click",d),o.addEventListener("click",d);
-//# sourceMappingURL=index.5a42af7b.js.map
--- index.e5ea480c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-modal-open]"),t=document.querySelector("[data-modal]"),o=document.querySelector("[data-modal-close]"),c=function(){t.classList.toggle("is-hidden"),document.body.classList.toggle("no__scroll")};e.addEventListener("click",c),o.addEventListener("click",c)}();
-//# sourceMappingURL=index.e5ea480c.js.map
